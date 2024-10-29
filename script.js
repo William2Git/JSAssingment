df7a74b3a29e18c34aba46d07032f27c40bd8031
William Weng <william.weng@student.tdsb.on.ca>
2024-10-29 11:40:28 -0400

    Added some js

---
+++ script.js
@@ -0,0 +1,25 @@
+
+// const button = document.getElementById("calc");
+
+
+
+// button.addEventListener("click", heron);
+function heron() {
+
+    const a = document.getElementById("a").value;
+    const b = document.getElementById("b").value;
+    const c = document.getElementById("c").value;
+
+    document.getElementById("answer").value = Math.sqrt(4*a*a*b*b - (a*a ))
+
+    console.log("hello world");
+    document.getElementById("calc").value = a;
+
+
+
+}
+
+
+
+
+
